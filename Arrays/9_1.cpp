a37765fdda880dce515eb8b621626b9e6f8e9608
Abhishek Singh <101743652+AbhishekSingh1247@users.noreply.github.com>
2022-09-28 01:16:53 +0530

    Add files via upload

---
+++ Arrays/9_1.cpp
@@ -0,0 +1,40 @@
+//MAX MIN OF ELEMENTS
+#include<iostream>
+#include <climits>
+using namespace std;
+int getMin(int arr[],int size){
+    int min  =INT_MAX; //  2^31-1
+    
+    for(int i=0;i<size;i++){
+
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    return min;
+}
+
+int getMax(int arr[],int size){
+    int max=  INT_MIN;   //  -2^31
+    for(int i=0;i<size;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+
+    }
+    return max;
+}
+
+int main(){
+    int size;
+    cin>>size;
+    int arr[100];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+
+        cout<<"Maximum element is "<<getMax(arr,size)<<"  "<<endl;
+        cout<<"Minimum element is"<<getMin(arr,size)<<" "<<endl;
+
+        return 0;
+    }
+}
